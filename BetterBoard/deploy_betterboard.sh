d81d432a5e2c454849e3416cafa6dc8aea97553e
Jared Pochtar <jpochtar@gmail.com>
2012-11-11 05:02:37 -0500

    removed BetterBoard; was old version

--- BetterBoard/deploy_betterboard.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-#scp -r -P 2282 /Users/Jared/Dropbox/Development/BetterBoard/ jared@hchs01.welinknyc.com:BBDeploymentStaging
-rsync -avz -r -e "ssh -p 2282" /Users/Jared/Dropbox/Development/BetterBoard/ jared@hchs01.welinknyc.com:~/BetterBoard
-ssh jared@hchs01.welinknyc.com -p 2282 /home/jared/deploy_betterboard.sh
